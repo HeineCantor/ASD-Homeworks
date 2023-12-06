2381eed82fd1ae4a614e73072ef4fddd3726a006
HeineCantor <carmine.palmese@gmail.com>
2023-12-06 18:27:16 +0100

    preappello solved

--- Practice/Preappello2022/main.py
+++ Practice/Preappello2022/main.py
@@ -1,17 +1,52 @@
 testProblem = [[8,5,3,2], [3,4,7,6]]
+#testProblem = [[2,5,3,9,12,4], [1,7,2,4,3,10]]
 associationList = []
 
-dp = []
+dp = {}
 counter = 0
 
-def findMaxLinks(sourceList, destinationList):
-    for i in range(len(associationList)):
-        tmpList = []
-        for j in range(len(sourceList)):
-            tmpList.append(-1)
-        dp.append(tmpList)
+def findMaxLinks(possibleLinks : list):
+    possibleLinks.sort()
 
-    print(dp)
+    return maxLinksDP(possibleLinks, len(possibleLinks)-1)
+
+'''
+Spiegazione: se si ordinano le source in ordine crescente,
+si otterrà un certo ordine di destination. Quelle che si
+possono prendere sono solo quelle maggiori tra loro, di
+conseguenza l'algoritmo si riduce a un Longest Increasing
+Subset (LIS). 
+
+Ad esempio, se ho dei collegamenti del tipo:
+
+(8->3), (5->4), (3->7), (2->6)
+ordino per destinazioni in ordine crescente:
+(2->6), (3->7), (5->4), (8->3)
+ed estraggo le destinazioni:
+6,7,4,3
+
+Devo quindi trovare il LIS di questa lista, che rappresenta
+il maggior numero di collegamenti che non si intersecano,
+perché sono tutti con S>S' and D>D'.
+'''
+
+def maxLinksDP(possibleLinks, n):
+    if n == 0:
+        return 1
+
+    maximum = 0
+    lastGreatestValue = -1
+
+    for i in range(n):
+        iMaximum = maxLinksDP(possibleLinks, i)
+        if (iMaximum > maximum):
+            maximum = iMaximum
+            lastGreatestValue = possibleLinks[i][1]
+
+    if (possibleLinks[n][1] > lastGreatestValue):
+        maximum += 1
+
+    return maximum
 
 if __name__ == "__main__":
     sourceList = testProblem[0]
@@ -20,7 +55,6 @@ if __name__ == "__main__":
     for pair in zip(testProblem[0], testProblem[1]):
         associationList.append(pair)
 
-    sourceList.sort()
-    destinationList.sort()
+    maximumLength = findMaxLinks(associationList)
 
-    findMaxLinks(sourceList, destinationList)
+    print(maximumLength)
